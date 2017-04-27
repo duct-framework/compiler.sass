baed40addaad14b1bf1b8a972425016a861aabba
James Reeves <jreeves@weavejester.com>
2017-04-27 20:15:53 +0100

    Add resume-key functionality
    No need to recompile files that haven't changed.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [integrant "0.4.0"]
-                 [io.bit3/jsass "5.5.0"]])
+                 [io.bit3/jsass "5.5.0"]
+                 [medley "1.0.0"]])
 
--- src/duct/compiler/sass.clj
+++ src/duct/compiler/sass.clj
@@ -3,7 +3,8 @@
            [io.bit3.jsass.context FileContext])
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [integrant.core :as ig]))
+            [integrant.core :as ig]
+            [medley.core :as m]))
 
 (def ^:private compiler (io.bit3.jsass.Compiler.))
 
@@ -23,10 +24,16 @@
       (str/replace re-ext ".css")
       (as-> f (io/file output-path f))))
 
-(defn- file-mapping [source-path output-path]
+(defn- file-mapping-1 [source-path output-path]
   (->> (find-files source-path ["scss" "sass"])
        (map (juxt identity #(css-output-file % source-path output-path)))
        (into {})))
+
+(defn- file-mapping [{:keys [source-paths output-path]}]
+  (into {} (map #(file-mapping-1 % output-path)) source-paths))
+
+(defn- timestamp-map [files]
+  (into {} (map (juxt identity #(.lastModified %)) files)))
 
 (defn- compile-sass [in out]
   (let [context (FileContext. (.toURI in) (.toURI out) (Options.))
@@ -34,9 +41,21 @@
     (.mkdirs (.getParentFile out))
     (spit out (.getCss result))))
 
+(defn- remove-unchanged [in->out file->timestamp]
+  (m/filter-keys #(some-> (file->timestamp %) (< (.lastModified %))) in->out))
+
+(defn- compile-results [in->out]
+  {:output     (map (comp str val) in->out)
+   :timestamps (timestamp-map (keys in->out))})
+
 (derive :duct.compiler/sass :duct/compiler)
 
-(defmethod ig/init-key :duct.compiler/sass [_ {:keys [source-paths output-path]}]
-  (let [in->out (into {} (map #(file-mapping % output-path)) source-paths)]
+(defmethod ig/init-key :duct.compiler/sass [_ opts]
+  (let [in->out (file-mapping opts)]
     (run! (fn [[in out]] (compile-sass in out)) in->out)
-    (map (comp str val) in->out)))
+    (compile-results in->out)))
+
+(defmethod ig/resume-key :duct.compiler/sass [key opts _ {:keys [timestamps]}]
+  (let [in->out (file-mapping opts)]
+    (run! (fn [[in out]] (compile-sass in out)) (remove-unchanged in->out timestamps))
+    (compile-results in->out)))
