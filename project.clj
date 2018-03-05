e20f3e5f7a923e4f283e96693d68c0839777f7ea
James Reeves <jreeves@weavejester.com>
2018-03-05 13:38:30 -0500

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/duct-framework/compiler.sass"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [duct/logger "0.1.1"]
-                 [integrant "0.4.0"]
-                 [io.bit3/jsass "5.5.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [duct/logger "0.2.1"]
+                 [integrant "0.6.3"]
+                 [io.bit3/jsass "5.5.6"]
                  [medley "1.0.0"]]
   :test-paths ["test" "target/test"])
 
