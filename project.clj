d8a36f302cc9b1ca8c8102370f54e27dcfb8aa4f
James Reeves <jreeves@weavejester.com>
2024-04-10 13:01:06 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [duct/logger "0.2.1"]
-                 [integrant "0.6.3"]
+                 [duct/logger "0.3.0"]
+                 [integrant "0.8.1"]
                  [io.bit3/jsass "5.11.0"]
-                 [medley "1.0.0"]]
+                 [dev.weavejester/medley "1.7.0"]]
   :profiles {:test {:dependencies [[org.clojure/data.json "2.5.0"]]}}
   :test-paths ["test" "target/test"])
 
